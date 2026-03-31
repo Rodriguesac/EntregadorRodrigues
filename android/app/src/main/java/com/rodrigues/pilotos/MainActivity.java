093257b867dfcf12d4c1bc77b5c34dacc7f56358
Rodriguesac <rodrigues.acaiecia@gmail.com>
2026-03-31 19:19:30 -0400

    Permissoes de GPS e Camera finais no RG

--- android/app/src/main/java/com/rodrigues/pilotos/MainActivity.java
+++ android/app/src/main/java/com/rodrigues/pilotos/MainActivity.java
@@ -1,5 +1,18 @@
-package com.rodrigues.pilotos;
+package com.rodrigues.pilotos; // Verifique se o seu package é este mesmo
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Registra os plugins nativos que o seu app usa
+    registerPlugin(com.capacitorjs.plugins.geolocation.GeolocationPlugin.class);
+    registerPlugin(com.capacitorjs.plugins.camera.CameraPlugin.class);
+    registerPlugin(com.capacitorjs.plugins.haptics.HapticsPlugin.class);
+    registerPlugin(com.capacitorjs.plugins.network.NetworkPlugin.class);
+  }
+}
